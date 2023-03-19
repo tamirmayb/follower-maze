d76c734e17546c89ba18b5cabd35b2e5cdffffd0
tamirmayblat <tamirmayb@gmail.com>
2023-03-19 14:17:13 +0200

    Upgrading to version 2

---
+++ src/main/java/com/follower/maze/event/events/EventType.java
@@ -0,0 +1,25 @@
+package com.follower.maze.event.events;
+
+public enum EventType {
+    FOLLOW ("F"),
+    UNFOLLOW ("U"),
+    BROADCAST ("B"),
+    PRIVATE_MSG ("P"),
+    STATUS_UPDATE ("S");
+
+    public final String typeCode;
+
+    EventType(String typeCode) {
+        this.typeCode = typeCode;
+    }
+
+    public static EventType valueOfTypeCode(String typeCode) {
+        for (EventType e : values()) {
+            if (e.typeCode.equals(typeCode)) {
+                return e;
+            }
+        }
+        return null;
+    }
+
+}
